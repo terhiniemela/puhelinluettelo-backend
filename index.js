c8740995abeadb39578096ea0376b660e2981fa7
Terhi Niemelä <terhi.niemela@gmail.com>
2024-07-21 18:48:32 +0300

    uibuild

--- index.js
+++ index.js
@@ -29,28 +29,8 @@ morgan.token('body', (request, response) => {
 // log configuration for 3.8.
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": "1"
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": "2"
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": "3"
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": "4"
-    }
-]
+// 
+let persons = []
 
 
 app.get('/', (request, response) => {
